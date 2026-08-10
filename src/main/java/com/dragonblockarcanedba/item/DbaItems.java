bbf670c80b53beef8ede5ffe18d9e866bf4e6ffa
mattjkrl-boop <mattjkrl@gmail.com>
2026-08-10 01:26:37 -0500

    items

--- src/main/java/com/dragonblockarcanedba/item/DbaItems.java
+++ src/main/java/com/dragonblockarcanedba/item/DbaItems.java
@@ -46,6 +46,84 @@ public class DbaItems {
     public static final ResourceKey<Item> FAC_POWER_KEY = ResourceKey.create(
         Registries.ITEM, DragonBlockArcaneDBA.id("fac_power")
     );
+    public static final ResourceKey<Item> T1_COIN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("t1_coin")
+    );
+    public static final ResourceKey<Item> T2_COIN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("t2_coin")
+    );
+    public static final ResourceKey<Item> TP_COIN_0_1_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("tp_coin_0_1")
+    );
+    public static final ResourceKey<Item> ORBITAL_COIN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("orbital_coin")
+    );
+    public static final ResourceKey<Item> TRADE_COIN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("trade_coin")
+    );
+    public static final ResourceKey<Item> GOLD_COIN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("gold_coin")
+    );
+    public static final ResourceKey<Item> SPIRIT_SWORD_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("spirit_sword")
+    );
+    public static final ResourceKey<Item> BLUE_ORB_1_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("blue_orb_1")
+    );
+    public static final ResourceKey<Item> SILVER_COIN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("silver_coin")
+    );
+    public static final ResourceKey<Item> BLUE_ORB_2_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("blue_orb_2")
+    );
+    public static final ResourceKey<Item> BLUE_KI_ORB_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("blue_ki_orb")
+    );
+    public static final ResourceKey<Item> MAJIN_MARK_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("majin_mark")
+    );
+    public static final ResourceKey<Item> XP_AURA_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("xp_aura")
+    );
+    public static final ResourceKey<Item> DNA_HELIX_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("dna_helix")
+    );
+    public static final ResourceKey<Item> GOLD_KI_SHARD_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("gold_ki_shard")
+    );
+    public static final ResourceKey<Item> PURPLE_SCYTHE_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("purple_scythe")
+    );
+    public static final ResourceKey<Item> TIME_COIN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("time_coin")
+    );
+    public static final ResourceKey<Item> RED_CROWN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("red_crown")
+    );
+    public static final ResourceKey<Item> BLUE_CROWN_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("blue_crown")
+    );
+    public static final ResourceKey<Item> PURPLE_KI_ORB_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("purple_ki_orb")
+    );
+    public static final ResourceKey<Item> RED_FRUIT_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("red_fruit")
+    );
+    public static final ResourceKey<Item> WHIS_STAFF_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("whis_staff")
+    );
+    public static final ResourceKey<Item> PURPLE_ORB_LARGE_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("purple_orb_large")
+    );
+    public static final ResourceKey<Item> PURPLE_ORB_SMALL_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("purple_orb_small")
+    );
+    public static final ResourceKey<Item> RED_TICKETS_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("red_tickets")
+    );
+    public static final ResourceKey<Item> DBA_LOGO_KEY = ResourceKey.create(
+        Registries.ITEM, DragonBlockArcaneDBA.id("dba_logo")
+    );
 
     public static final Item SPACE_POD = new SpacePodItem(
         new Item.Properties().setId(SPACE_POD_KEY).stacksTo(1)
@@ -81,10 +159,88 @@ public class DbaItems {
     public static final Item FAC_POWER = new Item(
         new Item.Properties().setId(FAC_POWER_KEY)
     );
+    public static final Item T1_COIN = new Item(
+        new Item.Properties().setId(T1_COIN_KEY)
+    );
+    public static final Item T2_COIN = new Item(
+        new Item.Properties().setId(T2_COIN_KEY)
+    );
+    public static final Item TP_COIN_0_1 = new Item(
+        new Item.Properties().setId(TP_COIN_0_1_KEY)
+    );
+    public static final Item ORBITAL_COIN = new Item(
+        new Item.Properties().setId(ORBITAL_COIN_KEY)
+    );
+    public static final Item TRADE_COIN = new Item(
+        new Item.Properties().setId(TRADE_COIN_KEY)
+    );
+    public static final Item GOLD_COIN = new Item(
+        new Item.Properties().setId(GOLD_COIN_KEY)
+    );
+    public static final Item SPIRIT_SWORD = new Item(
+        new Item.Properties().setId(SPIRIT_SWORD_KEY)
+    );
+    public static final Item BLUE_ORB_1 = new Item(
+        new Item.Properties().setId(BLUE_ORB_1_KEY)
+    );
+    public static final Item SILVER_COIN = new Item(
+        new Item.Properties().setId(SILVER_COIN_KEY)
+    );
+    public static final Item BLUE_ORB_2 = new Item(
+        new Item.Properties().setId(BLUE_ORB_2_KEY)
+    );
+    public static final Item BLUE_KI_ORB = new Item(
+        new Item.Properties().setId(BLUE_KI_ORB_KEY)
+    );
+    public static final Item MAJIN_MARK = new Item(
+        new Item.Properties().setId(MAJIN_MARK_KEY)
+    );
+    public static final Item XP_AURA = new Item(
+        new Item.Properties().setId(XP_AURA_KEY)
+    );
+    public static final Item DNA_HELIX = new Item(
+        new Item.Properties().setId(DNA_HELIX_KEY)
+    );
+    public static final Item GOLD_KI_SHARD = new Item(
+        new Item.Properties().setId(GOLD_KI_SHARD_KEY)
+    );
+    public static final Item PURPLE_SCYTHE = new Item(
+        new Item.Properties().setId(PURPLE_SCYTHE_KEY)
+    );
+    public static final Item TIME_COIN = new Item(
+        new Item.Properties().setId(TIME_COIN_KEY)
+    );
+    public static final Item RED_CROWN = new Item(
+        new Item.Properties().setId(RED_CROWN_KEY)
+    );
+    public static final Item BLUE_CROWN = new Item(
+        new Item.Properties().setId(BLUE_CROWN_KEY)
+    );
+    public static final Item PURPLE_KI_ORB = new Item(
+        new Item.Properties().setId(PURPLE_KI_ORB_KEY)
+    );
+    public static final Item RED_FRUIT = new Item(
+        new Item.Properties().setId(RED_FRUIT_KEY)
+    );
+    public static final Item WHIS_STAFF = new Item(
+        new Item.Properties().setId(WHIS_STAFF_KEY)
+    );
+    public static final Item PURPLE_ORB_LARGE = new Item(
+        new Item.Properties().setId(PURPLE_ORB_LARGE_KEY)
+    );
+    public static final Item PURPLE_ORB_SMALL = new Item(
+        new Item.Properties().setId(PURPLE_ORB_SMALL_KEY)
+    );
+    public static final Item RED_TICKETS = new Item(
+        new Item.Properties().setId(RED_TICKETS_KEY)
+    );
+    public static final Item DBA_LOGO = new Item(
+        new Item.Properties().setId(DBA_LOGO_KEY)
+    );
 
     public static final CreativeModeTab DBA_TAB = CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
         .title(Component.translatable("itemGroup.dragonblockarcanedba.dba_items"))
-        .icon(() -> new net.minecraft.world.item.ItemStack(DBA_ORB))
+        .icon(() -> new net.minecraft.world.item.ItemStack(DBA_LOGO))
         .displayItems((itemDisplayParameters, output) -> {
             output.accept(SPACE_POD);
             output.accept(BRONZE_COIN);
@@ -97,6 +253,31 @@ public class DbaItems {
             output.accept(FAC_COIN);
             output.accept(FAC_LOOP);
             output.accept(FAC_POWER);
+            output.accept(T1_COIN);
+            output.accept(T2_COIN);
+            output.accept(TP_COIN_0_1);
+            output.accept(ORBITAL_COIN);
+            output.accept(TRADE_COIN);
+            output.accept(GOLD_COIN);
+            output.accept(SPIRIT_SWORD);
+            output.accept(BLUE_ORB_1);
+            output.accept(SILVER_COIN);
+            output.accept(BLUE_ORB_2);
+            output.accept(BLUE_KI_ORB);
+            output.accept(MAJIN_MARK);
+            output.accept(XP_AURA);
+            output.accept(DNA_HELIX);
+            output.accept(GOLD_KI_SHARD);
+            output.accept(PURPLE_SCYTHE);
+            output.accept(TIME_COIN);
+            output.accept(RED_CROWN);
+            output.accept(BLUE_CROWN);
+            output.accept(PURPLE_KI_ORB);
+            output.accept(RED_FRUIT);
+            output.accept(WHIS_STAFF);
+            output.accept(PURPLE_ORB_LARGE);
+            output.accept(PURPLE_ORB_SMALL);
+            output.accept(RED_TICKETS);
         })
         .build();
 
@@ -112,6 +293,32 @@ public class DbaItems {
         Registry.register(BuiltInRegistries.ITEM, FAC_COIN_KEY, FAC_COIN);
         Registry.register(BuiltInRegistries.ITEM, FAC_LOOP_KEY, FAC_LOOP);
         Registry.register(BuiltInRegistries.ITEM, FAC_POWER_KEY, FAC_POWER);
+        Registry.register(BuiltInRegistries.ITEM, T1_COIN_KEY, T1_COIN);
+        Registry.register(BuiltInRegistries.ITEM, T2_COIN_KEY, T2_COIN);
+        Registry.register(BuiltInRegistries.ITEM, TP_COIN_0_1_KEY, TP_COIN_0_1);
+        Registry.register(BuiltInRegistries.ITEM, ORBITAL_COIN_KEY, ORBITAL_COIN);
+        Registry.register(BuiltInRegistries.ITEM, TRADE_COIN_KEY, TRADE_COIN);
+        Registry.register(BuiltInRegistries.ITEM, GOLD_COIN_KEY, GOLD_COIN);
+        Registry.register(BuiltInRegistries.ITEM, SPIRIT_SWORD_KEY, SPIRIT_SWORD);
+        Registry.register(BuiltInRegistries.ITEM, BLUE_ORB_1_KEY, BLUE_ORB_1);
+        Registry.register(BuiltInRegistries.ITEM, SILVER_COIN_KEY, SILVER_COIN);
+        Registry.register(BuiltInRegistries.ITEM, BLUE_ORB_2_KEY, BLUE_ORB_2);
+        Registry.register(BuiltInRegistries.ITEM, BLUE_KI_ORB_KEY, BLUE_KI_ORB);
+        Registry.register(BuiltInRegistries.ITEM, MAJIN_MARK_KEY, MAJIN_MARK);
+        Registry.register(BuiltInRegistries.ITEM, XP_AURA_KEY, XP_AURA);
+        Registry.register(BuiltInRegistries.ITEM, DNA_HELIX_KEY, DNA_HELIX);
+        Registry.register(BuiltInRegistries.ITEM, GOLD_KI_SHARD_KEY, GOLD_KI_SHARD);
+        Registry.register(BuiltInRegistries.ITEM, PURPLE_SCYTHE_KEY, PURPLE_SCYTHE);
+        Registry.register(BuiltInRegistries.ITEM, TIME_COIN_KEY, TIME_COIN);
+        Registry.register(BuiltInRegistries.ITEM, RED_CROWN_KEY, RED_CROWN);
+        Registry.register(BuiltInRegistries.ITEM, BLUE_CROWN_KEY, BLUE_CROWN);
+        Registry.register(BuiltInRegistries.ITEM, PURPLE_KI_ORB_KEY, PURPLE_KI_ORB);
+        Registry.register(BuiltInRegistries.ITEM, RED_FRUIT_KEY, RED_FRUIT);
+        Registry.register(BuiltInRegistries.ITEM, WHIS_STAFF_KEY, WHIS_STAFF);
+        Registry.register(BuiltInRegistries.ITEM, PURPLE_ORB_LARGE_KEY, PURPLE_ORB_LARGE);
+        Registry.register(BuiltInRegistries.ITEM, PURPLE_ORB_SMALL_KEY, PURPLE_ORB_SMALL);
+        Registry.register(BuiltInRegistries.ITEM, RED_TICKETS_KEY, RED_TICKETS);
+        Registry.register(BuiltInRegistries.ITEM, DBA_LOGO_KEY, DBA_LOGO);
         Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, DBA_TAB_KEY, DBA_TAB);
         DragonBlockArcaneDBA.LOGGER.info("Registered DBA items");
     }
