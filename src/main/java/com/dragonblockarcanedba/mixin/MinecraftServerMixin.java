20205b3c9520b15144597087b825c2146ad21908
Carter Moyer <moyer6141@uwlax.edu>
2026-08-20 12:22:35 -0500

    multiplayer testing

--- src/main/java/com/dragonblockarcanedba/mixin/MinecraftServerMixin.java
+++ src/main/java/com/dragonblockarcanedba/mixin/MinecraftServerMixin.java
@@ -1,19 +1,19 @@
-package com.dragonblockarcanedba.mixin.client;
+package com.dragonblockarcanedba.mixin;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(IntegratedServer.class)
-public class IntegratedServerMixin {
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
     @Inject(method = "usesAuthentication", at = @At("HEAD"), cancellable = true)
     private void dba$disableLanOnlineMode(CallbackInfoReturnable<Boolean> cir) {
-        // Only disable Mojang session server verification when running in the development / script launch environment
-        // so normal launcher / production downloads keep full vanilla Mojang authentication intact.
-        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+        MinecraftServer server = (MinecraftServer) (Object) this;
+        // Only disable authentication for Integrated/LAN servers when running in the dev/script environment
+        if (!server.isDedicatedServer() && FabricLoader.getInstance().isDevelopmentEnvironment()) {
             cir.setReturnValue(false);
         }
     }
